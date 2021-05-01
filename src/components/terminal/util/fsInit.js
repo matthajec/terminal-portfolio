c915c9b2549e03a4170d95c15fb79902d62857a8
matthajec <66030008+matthajec@users.noreply.github.com>
2021-05-01 10:07:13 -0400

    added more files to projects folder

--- src/components/terminal/util/fsInit.js
+++ src/components/terminal/util/fsInit.js
@@ -34,7 +34,63 @@ const rootFs = new FSNode(
               'Check out the readme on github for more info'
             ]
           )
-        )
+        ),
+        new FSNode(
+          'movies.txt',
+          null,
+          new TextLine(
+            'standard',
+            [
+              'Movie Search App',
+              'https://movies.matthajec.com',
+              'https://github.com/matthajec/movie-search-rebuild',
+              '',
+              'This is a "full" stack project built with React and NodeJs. The backend is the just a sort of proxy, it queries the RESTful API and sends back the results so that the API key is not exposed. In the front end, you can see trending movies, search for movies, and see information about movies.'
+            ]
+          )
+        ),
+        new FSNode(
+          'hackernews.txt',
+          null,
+          new TextLine(
+            'standard',
+            [
+              'Hacker news app',
+              'https://hackernews.matthajec.com',
+              'https://github.com/matthajec/hacker-news-clone',
+              '',
+              'This is a front end project built with React that shows articles from hackernews. You can sort by best, top, or new articles.']
+            ]
+          )
+        ),
+        new FSNode(
+          'sorting-algorithms.txt',
+          null,
+          new TextLine(
+            'standard',
+            [
+              'Simple Sorting Algorithms',
+              'https://www.sorting-algorithms.matthajec.com/',
+              'https://github.com/matthajec/simple-sorting-algoriths',
+              '',
+              'This is a front end project built with Bootstrap and jQuery that shows the user a visualization of the bubble, selection, and insertion sorting algorithms.']
+            ]
+          )
+        ),
+        new FSNode(
+          'whereintheworld.txt',
+          null,
+          new TextLine(
+            'standard',
+            [
+              'Where in the world?',
+              'https://www.whereintheworld.matthajec.com/',
+              'https://github.com/matthajec/react-countries-app',
+              '',
+              'This is a front end project built with React that arguably shows all of the worlds countries, you can sort by region and/or search by name.']
+            ]
+          )
+        ),
       ]
     )
   ]
